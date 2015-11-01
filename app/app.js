71b1b63bd675a7968ac76c5bd3f25d3bf17b333a
Волков Пётр <p.v.volkov@yandex.ru>
2015-11-01 21:30:33 +0300

    add angular-route

---
+++ app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', ['ngRoute']).config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/view1', {
+        templateUrl: '/app/view1/view1.html'
+    });
+    $routeProvider.when('/view2', {
+        templateUrl: '/app/view2/view2.html'
+    });
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+});
